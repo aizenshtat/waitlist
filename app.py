3d6f816a4e690a10e248f3ce408a269a7d011c0a
Daniil <daniil.aizenshtat@gmail.com>
2022-12-18 15:02:36 +0000

    Add Initial Waitlist app
    Initially simple app with only three features: Add to waitlist, view waitlist, proceed to next entry.

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+wait_list = []
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+  if request.method == "POST":
+    if request.form["submit_button"] == "Add to Wait List":
+      name = request.form["name"]
+      wait_list.append(name)
+    elif request.form["submit_button"] == "Next in Wait List":
+      next_item = wait_list[0]
+      wait_list.pop(0)
+  return render_template("index.html", wait_list=wait_list)
+
+if __name__ == "__main__":
+  app.run()
